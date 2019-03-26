b5eb4868c0f225b8493c05ed44f17af7a68d5a3b
Mariusz Bojkowski <mariusz.bojkowski@gmail.com>
2019-03-26 05:18:56 +0100

    Introduce AzureVariableAttribute

---
+++ source/Nuke.Useful/AzureVariableAttribute.cs
@@ -0,0 +1,26 @@
+﻿using Nuke.Common;
+using System;
+using System.Reflection;
+
+namespace Nuke.Useful
+{
+    public class AzureVariableAttribute : ParameterAttribute
+    {
+        private readonly string _name;
+
+        public AzureVariableAttribute(string variableName = null) => _name = variableName;
+
+        public override object GetValue(MemberInfo member, object instance)
+        {
+            var name = _name ?? member.Name;
+            var value = Environment.GetEnvironmentVariable(name?.ToUpper());
+
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("Azure variable is not defined: " + name);
+            }
+
+            return value;
+        }
+    }
+}
