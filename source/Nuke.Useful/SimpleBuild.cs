d3f34f4eab15b5faf9bd1a2296dba0ee80962234
Mariusz Bojkowski <mariusz.bojkowski@gmail.com>
2019-04-01 10:12:10 +0200

    Introduce SimpleBuild

---
+++ source/Nuke.Useful/SimpleBuild.cs
@@ -0,0 +1,47 @@
+﻿using Nuke.Common;
+using Nuke.Common.Tools.GitVersion;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.ProjectModel;
+
+namespace Nuke.Useful
+{
+    public class SimpleBuild : NukeBuild
+    {
+        [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+        public readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+        [GitVersion] public readonly GitVersion GitVersion;
+        public AbsolutePath SourceDirectory => RootDirectory / "source";
+        public AbsolutePath OutputDirectory => RootDirectory / "output";
+
+        [Solution] readonly Solution Solution;
+
+        Target Clean => _ => _
+            .Executes(() =>
+            {
+                SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+                EnsureCleanDirectory(OutputDirectory);
+            });
+
+        Target Restore => _ => _
+            .DependsOn(Clean)
+            .Executes(() => DotNetRestore(s => s.SetProjectFile(Solution)));
+
+        Target Compile => _ => _
+            .DependsOn(Restore)
+            .Executes(() =>
+            {
+                DotNetBuild(s => s
+                    .SetProjectFile(Solution)
+                    .SetConfiguration(Configuration)
+                    .SetAssemblyVersion(GitVersion.GetNormalizedAssemblyVersion())
+                    .SetFileVersion(GitVersion.GetNormalizedFileVersion())
+                    .SetInformationalVersion(GitVersion.InformationalVersion)
+                    .EnableNoRestore());
+            });
+    }
+}
