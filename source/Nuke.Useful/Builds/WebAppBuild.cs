14329353e974316bd5d46a82bf3f73fecc15f973
Mariusz Bojkowski <mariusz.bojkowski@gmail.com>
2019-05-04 06:15:36 +0200

    Introduce WebAppBuild

---
+++ source/Nuke.Useful/Builds/WebAppBuild.cs
@@ -0,0 +1,45 @@
+﻿using Nuke.Common;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Useful.Attributes;
+using System;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+namespace Nuke.Useful.Builds
+{
+    public abstract class WebAppBuild : SimpleBuild
+    {
+        [ArtifactDirectoryAzureVariable] protected string ArtifactOutputDirectory { get; }
+
+        protected abstract string PublishOutputDirectoryName { get; }
+
+        AbsolutePath PublishOutput;
+        protected Target PublishWeb => _ => _
+            .DependsOn(Compile)
+            .Executes(() =>
+            {
+                PublishOutput = OutputDirectory / PublishOutputDirectoryName;
+                EnsureExistingDirectory(PublishOutput);
+                DotNetPublish(p => p
+                    .SetWorkingDirectory(SourceDirectory)
+                    .SetConfiguration(Configuration)
+                    .EnableNoBuild()
+                    .SetOutput(PublishOutput));
+            });
+
+        protected Target SaveWebArtifacts => _ => _
+            .DependsOn(PublishWeb)
+            .Requires(() => ArtifactOutputDirectory)
+            .Executes(() =>
+            {
+                if (PublishOutput is null)
+                {
+                    throw new ArgumentNullException(nameof(PublishOutput));
+                }
+
+                ArtifactStorage.Create(ArtifactOutputDirectory).AddDirectory(PublishOutput);
+            });
+    }
+}
