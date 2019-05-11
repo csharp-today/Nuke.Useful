c8ddaef22ec6ad601ab195a245807df36ae16230
Mariusz Bojkowski <mariusz.bojkowski@gmail.com>
2019-05-11 15:53:53 +0200

    Introduce AzureWebAppRelease

---
+++ source/Nuke.Useful/Builds/AzureWebAppRelease.cs
@@ -0,0 +1,87 @@
+﻿using AzureUploader;
+using FluentFTP;
+using Microsoft.Azure.Management.AppService.Fluent;
+using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
+using Nuke.Common;
+using Nuke.Useful.Attributes;
+using System.IO;
+using System.Net;
+
+namespace Nuke.Useful.Builds
+{
+    public abstract class AzureWebAppRelease : NukeBuild
+    {
+        [AzureVariable] protected string AzureClientId { get; }
+        [AzureVariable] protected string AzureSecret { get; }
+        [AzureVariable] protected string AzureSubscriptionId { get; }
+        [AzureVariable] protected string AzureTenantId { get; }
+
+        [AzureVariable] protected string AzureDeploySecret { get; }
+        [AzureVariable] protected string AzureDeployUrl { get; }
+        [AzureVariable] protected string AzureDeployUser { get; }
+
+        protected abstract string AzureResourceGroupName { get; }
+        protected abstract string AzureWebAppName { get; }
+        protected abstract string PublishOutputDirectoryName { get; }
+
+        IWebApp WebApp;
+        protected Target ConnectToAzureWebApp => _ => _
+            .Requires(() => AzureClientId)
+            .Requires(() => AzureSecret)
+            .Requires(() => AzureSubscriptionId)
+            .Requires(() => AzureTenantId)
+            .Executes(() =>
+            {
+                var credentials = new AzureCredentialsFactory()
+                    .FromServicePrincipal(AzureClientId, AzureSecret, AzureTenantId, AzureEnvironment.AzureGlobalCloud);
+                Logger.Info("Azure credentials prepared");
+                var azure = Azure.Authenticate(credentials).WithSubscription(AzureSubscriptionId);
+                Logger.Info("Connected to Azure");
+                WebApp = azure.WebApps.GetByResourceGroup(AzureResourceGroupName, AzureWebAppName);
+                Logger.Info("Connected to web app");
+            });
+
+        protected Target StopWebSite => _ => _
+            .DependsOn(ConnectToAzureWebApp)
+            .Executes(() =>
+            {
+                WebApp.Stop();
+                Logger.Info("Web app stopped");
+            });
+
+        protected Target Publish => _ => _
+            .DependsOn(StopWebSite)
+            .Requires(() => AzureDeployUrl)
+            .Requires(() => AzureDeployUser)
+            .Requires(() => AzureDeploySecret)
+            .Executes(() =>
+            {
+                new AzureFtpUploader(() => new FtpClient(AzureDeployUrl)
+                {
+                    Credentials = new NetworkCredential(AzureDeployUser, AzureDeploySecret)
+                }, new LogAdapter()).Deploy(PublishOutputDirectoryName);
+            });
+
+        protected Target StartWebSite => _ => _
+            .DependsOn(Publish)
+            .Executes(() =>
+            {
+                WebApp.Start();
+                Logger.Info("Web app started");
+            });
+
+        protected Target DeployAzureWebApp => _ => _
+            .DependsOn(StartWebSite);
+
+        protected static void EnsureNukeConfigFileIsPresent()
+        {
+            const string ConfigFile = ".nuke";
+            if (!File.Exists(ConfigFile))
+            {
+                File.WriteAllText(ConfigFile, "");
+            }
+        }
+    }
+}
---
+++ source/Nuke.Useful/LogAdapter.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+using Nuke.Common;
+using System;
+
+namespace Nuke.Useful
+{
+    internal class LogAdapter : ILogger
+    {
+        class Dummy : IDisposable
+        {
+            public void Dispose() { }
+        }
+
+        private static Dummy _dummy = new Dummy();
+
+        public IDisposable BeginScope<TState>(TState state) => _dummy;
+
+        public bool IsEnabled(Microsoft.Extensions.Logging.LogLevel logLevel) => true;
+
+        public void Log<TState>(Microsoft.Extensions.Logging.LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            var nukeLevel = logLevel switch
+            {
+                Microsoft.Extensions.Logging.LogLevel.Critical => Common.LogLevel.Error,
+                Microsoft.Extensions.Logging.LogLevel.Error => Common.LogLevel.Error,
+                Microsoft.Extensions.Logging.LogLevel.Information => Common.LogLevel.Normal,
+                Microsoft.Extensions.Logging.LogLevel.None => Common.LogLevel.Normal,
+                Microsoft.Extensions.Logging.LogLevel.Trace => Common.LogLevel.Trace,
+                Microsoft.Extensions.Logging.LogLevel.Debug => Common.LogLevel.Trace,
+                Microsoft.Extensions.Logging.LogLevel.Warning => Common.LogLevel.Warning,
+                _ => throw new ArgumentException("Unknown log level: " + logLevel)
+            };
+            Logger.Log(nukeLevel, $"{eventId.Id} {formatter(state, exception)}");
+        }
+    }
+}
