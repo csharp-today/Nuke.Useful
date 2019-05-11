4671d80a846e99de20301e4f19ba3bbd1d0948f6
Mariusz Bojkowski <mariusz.bojkowski@gmail.com>
2019-05-11 07:10:45 +0200

    Update NuGets & project description

--- build/Build.cs
+++ build/Build.cs
@@ -5,10 +5,5 @@ using Nuke.Useful.Builds;
 [UnsetVisualStudioEnvironmentVariables]
 class Build : AzureDevOpsLibraryBuild
 {
-    protected override string ArtifactOutputDirectory { get; }
-    protected override string FeedSecret { get; }
-    protected override string FeedUrl { get; }
-    protected override string FeedUser { get; }
-
     public static int Main() => Execute<Build>(x => x.BuildAzureDevOpsLibrary);
 }
