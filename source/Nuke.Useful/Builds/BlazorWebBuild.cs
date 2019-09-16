909128c39c54d2e5df30d3e4a867c29a3b8acd5f
Mariusz Bojkowski <mariusz.bojkowski@gmail.com>
2019-09-16 03:00:16 +0200

    Introduce BlazorWebBuild

---
+++ source/Nuke.Useful/Builds/BlazorWebBuild.cs
@@ -0,0 +1,22 @@
+﻿using Nuke.Common;
+using System;
+using System.IO;
+
+namespace Nuke.Useful.Builds
+{
+    public abstract class BlazorWebBuild : WebAppBuild
+    {
+        protected abstract string BlazorAssemblyName { get; }
+
+        protected string HostingRootDirectory { get; set; } = @"D:\home\site\wwwroot";
+
+        protected Target GenerateBlazorConfig => _ => _
+            .DependsOn(SaveWebArtifacts)
+            .Executes(() =>
+            {
+                var configPath = Path.Combine(ArtifactOutputDirectory, PublishOutputDirectoryName, $"{BlazorAssemblyName}.blazor.config");
+                var content = $@"{HostingRootDirectory}\{BlazorAssemblyName}\fake.csproj{Environment.NewLine}fake.dll";
+                File.WriteAllText(configPath, content);
+            });
+    }
+}
