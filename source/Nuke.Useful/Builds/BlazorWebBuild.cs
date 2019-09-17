6a404173fa2f75a0a9eb95b05ea36b05304aac08
Mariusz Bojkowski <mariusz.bojkowski@gmail.com>
2019-09-17 03:16:51 +0200

    Fix web.config in Blazor Web Build

--- source/Nuke.Useful/Builds/BlazorWebBuild.cs
+++ source/Nuke.Useful/Builds/BlazorWebBuild.cs
@@ -1,5 +1,6 @@
 ﻿using Nuke.Common;
 using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Nuke.Useful.Builds
@@ -18,5 +19,42 @@ namespace Nuke.Useful.Builds
                 var content = $@"{HostingRootDirectory}\{BlazorAssemblyName}\fake.csproj{Environment.NewLine}fake.dll";
                 File.WriteAllText(configPath, content);
             });
+
+        protected Target FixWebConfig => _ => _
+            .DependsOn(GenerateBlazorConfig)
+            .Executes(() =>
+            {
+                var configPath = Path.Combine(ArtifactOutputDirectory, PublishOutputDirectoryName, "web.config");
+                var lines = File.ReadAllText(configPath).Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+                var acceptedLines = new List<string>();
+                var inRewrite = false;
+                foreach (var line in lines)
+                {
+                    if (inRewrite)
+                    {
+                        if (line.Trim().Equals("</rewrite>", StringComparison.OrdinalIgnoreCase))
+                        {
+                            inRewrite = false;
+                        }
+                    }
+                    else
+                    {
+                        if (line.Trim().Equals("<rewrite>", StringComparison.OrdinalIgnoreCase))
+                        {
+                            inRewrite = true;
+                        }
+                        else
+                        {
+                            acceptedLines.Add(line);
+                        }
+                    }
+                }
+
+                var correctedContent = string.Join(Environment.NewLine, acceptedLines);
+                File.WriteAllText(configPath, correctedContent);
+            });
+
+        protected Target BuildBlazorWebApp => _ => _.DependsOn(FixWebConfig);
     }
 }
