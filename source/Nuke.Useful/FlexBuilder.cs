268653e07d15e6d2b88f68a89b15de8246eaa2b7
Mariusz Bojkowski <mariusz.bojkowski@gmail.com>
2019-09-30 11:10:33 +0200

    Introduce FlexBuilder

---
+++ source/Nuke.Useful/FlexBuilder.cs
@@ -0,0 +1,31 @@
+﻿using Nuke.Common;
+using System;
+using System.Collections.Generic;
+
+namespace Nuke.Useful
+{
+    public class FlexBuilder : NukeBuild
+    {
+        private List<Func<int>> _builds = new List<Func<int>>();
+
+        public FlexBuilder Add<T>() where T : NukeBuild, IFlexBuild
+        {
+            _builds.Add(() => Execute<T>(x => x.RunAll));
+            return this;
+        }
+
+        public int Run()
+        {
+            foreach (var buildFunc in _builds)
+            {
+                int exitCode = buildFunc();
+                if (exitCode != 0)
+                {
+                    return exitCode;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ source/Nuke.Useful/IFlexBuild.cs
@@ -0,0 +1,9 @@
+﻿using Nuke.Common;
+
+namespace Nuke.Useful
+{
+    public interface IFlexBuild
+    {
+        Target RunAll { get; }
+    }
+}
